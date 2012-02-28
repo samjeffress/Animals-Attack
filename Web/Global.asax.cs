dad074c287329f5e26e846865dc92ea8aba291a6
Sam Jeffress <samjeffress@gmail.com>
2012-02-28 12:06:46 +1100

    changed reference to raven (was copied local, corrected to use NuGet package

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Raven.Abstractions.Data;
@@ -15,7 +11,7 @@ namespace Web
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         public MvcApplication()
         {
@@ -52,7 +48,6 @@ namespace Web
                 "{controller}/{action}/{id}", // URL with parameters
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
-
         }
 
         protected void Application_Start()
